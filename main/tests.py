0f300530c08c344b4b062d9b24883806a6dd983f
hungngocphat01 <hungngocphat01@gmail.com>
2022-06-11 14:56:10 +0700

    Add unittest

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,29 @@
+import secrets
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import *
 
-# Create your tests here.
+
+class UserProfileTest(TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.test_username = secrets.token_hex(10)
+
+    def setUp(self):
+        u = User.objects.create(username=self.test_username)
+        u.save()
+
+    def test_profile_trigger(self):
+        # A profile should be created when a user is created
+        self.assertTrue(Profile.objects.filter(
+            user__username=self.test_username).exists(), 'Profile was not created')
+
+        # The corresponding profile should be deleted when a user is deleted
+        User.objects.get(username=self.test_username).delete()
+        self.assertFalse(Profile.objects.filter(
+            user__username=self.test_username).exists(), 'Profile was not deleted')
+
+        # All posts should be deleted as well
+        self.assertFalse(Post.objects.filter(user__username=self.test_username).exists(),
+                         'Corresponding posts was not deleted')
