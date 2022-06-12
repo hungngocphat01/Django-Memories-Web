9c6f520d602424e6d9e4f43dbdb9561f633ed37a
hungngocphat01 <hungngocphat01@gmail.com>
2022-06-12 10:34:29 +0700

    Add view post feature

--- main/urls.py
+++ main/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', login_required(HomeView.as_view()), name='home'),
     path('login', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
-    path('post_cr', login_required(PostCreateEditView.as_view()), name='post_create_edit')
+    path('post', login_required(PostCreateEditView.as_view()), name='post_create_edit'),
+    path('post/<str:post_id>', login_required(PostView.as_view()), name='post_view')
 ]
--- main/views.py
+++ main/views.py
@@ -1,12 +1,11 @@
-from email import message
-import re
 import django.contrib.messages as messages
-from django.views.generic import View
-from django.http import HttpRequest, JsonResponse
-from django.shortcuts import render, redirect
 from django.contrib.auth import logout as auth_logout
+from django.core.exceptions import *
 from django.db import transaction
-from requests import post
+from django.http import (Http404, HttpRequest, HttpResponseServerError)
+from django.shortcuts import redirect, render
+from django.views.generic import View
+
 from .forms import *
 
 
@@ -17,14 +16,17 @@ class HomeView(View):
     template_name = 'home/home.html'
 
     def get(self, request: HttpRequest):
-        current_user = request.user
-        posts = Post.objects.filter(user__username=current_user.username)
+        try:
+            current_user = request.user
+            posts = Post.objects.filter(user__username=current_user.username)
 
-        return render(request, self.template_name, {
-            'posts': posts,
-            'title': 'Home',
-            'current_user': current_user
-        })
+            return render(request, self.template_name, {
+                'posts': posts,
+                'title': 'Home',
+                'current_user': current_user
+            })
+        except Exception:
+            return HttpResponseServerError()
 
 
 class LoginView(View):
@@ -34,11 +36,14 @@ class LoginView(View):
     template_name = 'home/login.html'
 
     def get(self, request: HttpRequest):
-        current_user = request.user
-        return render(request, self.template_name, {
-            'title': 'Login',
-            'current_user': current_user
-        })
+        try:
+            current_user = request.user
+            return render(request, self.template_name, {
+                'title': 'Login',
+                'current_user': current_user
+            })
+        except:
+            return HttpResponseServerError()
 
 
 class LogoutView(View):
@@ -47,8 +52,11 @@ class LogoutView(View):
     """
 
     def get(self, request):
-        auth_logout(request)
-        return redirect('app:login')
+        try:
+            auth_logout(request)
+            return redirect('app:login')
+        except:
+            return HttpResponseServerError()
 
 
 class PostCreateEditView(View):
@@ -58,30 +66,64 @@ class PostCreateEditView(View):
     template_name = 'post/post_create_edit.html'
 
     def get(self, request: HttpRequest):
-        current_user = request.user
-
-        form = PostForm()
-        return render(request, self.template_name, {
-            'action': 'Create new',
-            'form': form,
-            'title': 'New post',
-            'current_user': current_user
-        })
+        try:
+            current_user = request.user
+            # Create new post for current user
+            form = PostForm()
+            return render(request, self.template_name, {
+                'action': 'Create new',
+                'form': form,
+                'title': 'New post',
+                'current_user': current_user
+            })
+        except:
+            return HttpResponseServerError()
 
     def post(self, request: HttpRequest):
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            with transaction.atomic():
-                post_obj = form.save(commit=False)
-                post_obj.user = request.user
-                post_obj.save()
-            messages.success(request, 'Successfully added a new post')
-            return redirect('app:home')
-        else:
+        try:
+            # Create new form
+            form = PostForm(request.POST, request.FILES)
+            # Validate and append to DB
+            if form.is_valid():
+                with transaction.atomic():
+                    post_obj = form.save(commit=False)
+                    post_obj.user = request.user
+                    post_obj.save()
+                messages.success(request, 'Successfully added a new post')
+                return redirect('app:home')
+            else:
+                raise ValidationError()
+        except ValidationError:
+            # Get all errors from form
             form_errors = form.errors.get_json_data()
+            # Send error messages to client
             for field in form_errors:
                 msgs = form_errors[field]
                 for msg in msgs:
                     render_msg = msg['message']
-                    messages.error(request, f'Error in field {field}: {render_msg}')
+                    messages.error(
+                        request, f'Error in field {field}: {render_msg}')
+            # Redirect to same page
             return redirect('app:post_create_edit')
+        except:
+            return HttpResponseServerError()
+
+
+class PostView(View):
+    template_name = 'post/view_post.html'
+
+    def get(self, request: HttpRequest, post_id: int):
+        try:
+            # Get requested post
+            current_user = request.user
+            post = Post.objects.get(
+                id=post_id, user__username=current_user.username)
+
+            # Render to client
+            return render(request, self.template_name, {
+                'post': post
+            })
+        except ObjectDoesNotExist:
+            raise Http404()
+        except:
+            return HttpResponseServerError()
